b0c8f89b5c9a480430991c20da224992b43f809d
Anton Viktorov <antvik85@gmail.com>
2020-06-26 12:31:49 +0300

    Added: API client wraper to support by region

--- src/AmazonAdvertisingApi.php
+++ src/AmazonAdvertisingApi.php
@@ -4,8 +4,15 @@ namespace Jagalan\AmazonAdvertisingApi;
 
 use AmazonAdvertisingApi\Client;
 
+/**
+ * Class AmazonAdvertisingApi
+ * @package Jagalan\AmazonAdvertisingApi
+ */
 class AmazonAdvertisingApi
 {
+    /**
+     * @return Client
+     */
     public function getClient()
     {
         $config = array(
@@ -14,6 +21,23 @@ class AmazonAdvertisingApi
             "refreshToken" => config('amazon-advertising-api.app.refreshToken'),
             "region" => config('amazon-advertising-api.app.region'),
             "sandbox" => config('amazon-advertising-api.app.sandbox'),
+        );
+
+        return new Client($config);
+    }
+
+    /**
+     * @param $region
+     * @return Client
+     */
+    public function getClientByRegion($region)
+    {
+        $config = array(
+            "clientId" => config('amazon-advertising-api.' . $region . '.clientId'),
+            "clientSecret" => config('amazon-advertising-api.' . $region . '.clientSecret'),
+            "refreshToken" => config('amazon-advertising-api.' . $region . '.refreshToken'),
+            "region" => config('amazon-advertising-api.' . $region . '.region'),
+            "sandbox" => config('amazon-advertising-api.' . $region . '.sandbox'),
         );
 
         return new Client($config);
