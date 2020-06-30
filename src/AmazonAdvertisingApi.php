14a4f9307dd1dd7d741290ad985f848d3a3f3e4a
Juan Galan Martinez <juan.galan.martinez@ebuynow.com>
2020-06-30 12:57:21 +0200

    Added V2 API client

--- src/AmazonAdvertisingApi.php
+++ src/AmazonAdvertisingApi.php
@@ -3,19 +3,33 @@
 namespace Jagalan\AmazonAdvertisingApi;
 
 use AmazonAdvertisingApi\Client;
+use Jagalan\AmazonAdvertisingApi\ClientV2;
 
 class AmazonAdvertisingApi
 {
-    public function getClient()
+    public function getClient($region = 'na')
     {
         $config = array(
-            "clientId" => config('amazon-advertising-api.app.clientId'),
-            "clientSecret" => config('amazon-advertising-api.app.clientSecret'),
-            "refreshToken" => config('amazon-advertising-api.app.refreshToken'),
-            "region" => config('amazon-advertising-api.app.region'),
-            "sandbox" => config('amazon-advertising-api.app.sandbox'),
+            "clientId" => config("amazon-advertising-api.$region.clientId"),
+            "clientSecret" => config("amazon-advertising-api.$region.clientSecret"),
+            "refreshToken" => config("amazon-advertising-api.$region.refreshToken"),
+            "region" => config("amazon-advertising-api.$region.region"),
+            "sandbox" => config("amazon-advertising-api.$region.sandbox"),
         );
 
         return new Client($config);
+    }
+
+    public function getClientV2($region = 'na')
+    {
+        $config = array(
+            "clientId" => config("amazon-advertising-api.$region.clientId"),
+            "clientSecret" => config("amazon-advertising-api.$region.clientSecret"),
+            "refreshToken" => config("amazon-advertising-api.$region.refreshToken"),
+            "region" => config("amazon-advertising-api.$region.region"),
+            "sandbox" => config("amazon-advertising-api.$region.sandbox"),
+        );
+
+        return new ClientV2($config);
     }
 }
---
+++ src/Versions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jagalan\AmazonAdvertisingApi;
+
+
+class Versions
+{
+    public $versionStrings = array(
+        "apiVersion"         => "v2",
+        "applicationVersion" => "1.3"
+    );
+}
